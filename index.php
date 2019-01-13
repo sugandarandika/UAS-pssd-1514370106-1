8d2bfe383cba8cf7123ed50358eaa957ffc4b2a1
Ganda <45309434+sugandarandika@users.noreply.github.com>
2019-01-13 17:36:36 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Sign-In</title>
+</head>
+<body id="body-color">
+<div id="Sign-In">
+<fieldset style="width:30%"><legend>Login Form</legend>
+<?php
+session_start();
+$sessData = !empty($_SESSION['sessData'])?$_SESSION['sessData']:'';
+if(!empty($sessData['status']['msg'])){
+    $statusMsg = $sessData['status']['msg'];
+    $statusMsgType = $sessData['status']['type'];
+    unset($_SESSION['sessData']['status']);
+}
+?>
+<div class="container">
+    <?php
+        if(!empty($sessData['userLoggedIn']) && !empty($sessData['userID'])){
+            include 'user.php';
+            $user = new User();
+            $conditions['where'] = array(
+                'id' => $sessData['userID'],
+            );
+            $conditions['return_type'] = 'single';
+            $userData = $user->getRows($conditions);
+    ?>
+    <h2>Welcome <?php echo $userData['first_name']; ?>!</h2>
+    <a href="userAccount.php?logoutSubmit=1" class="logout">Logout</a>
+    <div class="regisFrm">
+        <p><b>Name: </b><?php echo $userData['first_name'].' '.$userData['last_name']; ?></p>
+        <p><b>Email: </b><?php echo $userData['email']; ?></p>
+    </div>
+    <?php }else{ ?>
+    <?php echo !empty($statusMsg)?'<p class="'.$statusMsgType.'">'.$statusMsg.'</p>':''; ?>
+    <div class="regisFrm">
+        <form action="userAccount.php" method="post">
+            <input type="email" name="email" placeholder="EMAIL" required="">
+            <input type="password" name="password" placeholder="PASSWORD" required="">
+            <div class="send-button">
+                <input type="submit" name="loginSubmit" value="LOGIN">
+            </div>
+        </form>
+        <p>Don't have an account? <a href="pendaftaran.php">Register</a></p>
+    </div>
+    <?php } ?>
+</div>
---
+++ pendaftaran.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Sign-Up</title>
+</head>
+<body id="body-color">
+<div id="Sign-Up">
+<fieldset style="width:30%"><legend>Registration Form</legend>
+<table border="0">
+<tr>
+<form method="POST" action="useraccount.php">
+<td>FirstName</td><td> <input type="text" name="first_name"></td>
+</tr>
+<tr>
+<td>LastName</td><td> <input type="text" name="last_name"></td>
+</tr>
+<tr>
+<td>UserName</td><td> <input type="text" name="user_name"></td>
+</tr>
+<tr>
+<tr>
+<td>Email</td><td> <input type="text" name="email"></td>
+</tr>
+
+<tr>
+<td>Password</td><td> <input type="password" name="password"></td>
+</tr>
+<tr>
+<td>Confirm Password </td><td><input type="password" name="confirm_password"></td>
+</tr>
+<tr>
+<td><input id="button" type="submit" name="signupSubmit" value="Sign-Up"></td>
+</tr>
+</form>
+</table>
+</fieldset>
+</div>
+</body>
+</html>
